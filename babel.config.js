8db7ed1b3b3839f7f334a610cb64300da2f37279
fahmi.yakub.flip <fahmi.yakub@flip.id>
2022-08-22 16:53:55 +0700

    create alias path

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        root: ['./src'],
+        alias: {
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@assets': './src/assets',
+          '@screens': './src/screens',
+          '@themes': './src/themes',
+          '@store': './src/store',
+          '@types': './src/types',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
