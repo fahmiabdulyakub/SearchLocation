a54c96a973df53760e277e51a252bb9142cf78ab
fahmi.yakub.flip <fahmi.yakub@flip.id>
2022-08-22 17:34:23 +0700

    create navigation

---
+++ src/Root.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import Colors from 'themes/Colors';
+import {Home} from 'screens';
+import {StackParams} from 'types/navigationType';
+
+const Stack = createNativeStackNavigator<StackParams>();
+
+export const Routes = () => {
+  return (
+    <NavigationContainer>
+      <SafeAreaView style={styles.SafeAreaView}>
+        <StatusBar backgroundColor={Colors.white} barStyle="dark-content" />
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  SafeAreaView: {
+    flex: 1,
+  },
+});
---
+++ src/types/NavigationType.ts
@@ -0,0 +1,3 @@
+export type StackParams = {
+  Home: any;
+};
