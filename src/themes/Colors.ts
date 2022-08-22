06b6950b0d91b85f7d35eba263fd2ac2975e1f77
fahmi.yakub.flip <fahmi.yakub@flip.id>
2022-08-22 17:00:07 +0700

    add colors

---
+++ src/themes/Colors.ts
@@ -0,0 +1,13 @@
+const Colors = {
+  light: '#f8f9fa',
+  blue: '#0ecad4',
+  black: '#020203',
+  white: '#FFFFFF',
+  light_grey: '#eaeaea',
+  background: '#F9F9F9',
+  green: '#479647',
+  gray: '#C2C2C8',
+  dark_gray: '#6c757d',
+};
+
+export default Colors;
